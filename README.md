bf355a4a280c0bbcc4b23ece8d4a526b08ba5a75
Jonas Linux Mint <jonasmkunzler@gmail.com>
2023-11-23 17:39:45 -0300

    Ajustando navbar

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+Check de online project at https://nextjs-14-sandbox.vercel.app/
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,10 +3,10 @@ import Search from "./Search";
 
 export default function Navbar() {
   return (
-    <nav className="bg-stone-800 p-4 flex justify-between flex-col md:flex-row sticky top-0 drop-shadow-xl">
+    <nav className="bg-stone-800 p-4 flex justify-between flex-col md:flex-row sticky top-0 drop-shadow-xl opacity-90">
      <h1 className="text-3xl font-bold text-white grid place-content-center mb-2 md:mb-0">
         <Link href="/">
-          WIKI
+          NEXTJS 14 SANDBOX
         </Link>
       </h1>
       <Search />
